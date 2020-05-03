7fd89ed5788c24d90c244bdd3abf2ef526a8dc08
jhaameehu <jamiuazeez49@gmail.com>
2020-05-02 23:09:29 -0700

    updated the app-js, api_index-js, added a chart to the app and updated the country picker

--- src/App.js
+++ src/App.js
@@ -7,21 +7,28 @@ import styles from './App.module.css';
 class App extends React.Component {
     state = { 
         data: {},
+        country: '',
     }
 
     async componentDidMount () {
         const fetchedData = await fetchData();
+
         this.setState({ data: fetchedData });
+    }
+    handleCountryChange = async ( country ) => {
+        const fetchedData = await fetchData(country);
+       
+        this.setState({ data: fetchData, country: country});
     }
 
 
     render() {
-        const { data } = this.state;
+        const { data, country } = this.state;
         return (
             <div className={styles.container}> 
                 <Cards data={data} />
-                <CountryPicker />
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart  data={data} country={country} /> 
             </div>
         )
     }
--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,12 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async (country) => {
     let changeableUrl = url;
+    
     if(country) {
         changeableUrl = `${url}/countries/${country}`;
     }
     try {
-        const {data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        const {data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
        
         return { confirmed, recovered, deaths, lastUpdate };
     }catch (error){
